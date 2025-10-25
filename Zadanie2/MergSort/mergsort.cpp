a3092198d632c1f12a5e12f0e9c62b43ef2aa2fc
Kacper Dawid <thekamyk2060@gmail.com>
2025-10-25 16:13:03 +0200

    dodano testy do mergesrot

--- Zadanie2/MergSort/mergsort.cpp
+++ Zadanie2/MergSort/mergsort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 template<typename T>
 void Merge(std::vector<T> &LeftArray, std::vector<T> &RightArray, std::vector<T> &x, int start);
@@ -26,6 +27,7 @@ void MergeSort(int first, int last, std::vector<T> &x) {
         RightArray[i] = x[middle + i];
     }
     
+    // REKURENCJA - zachowanie spójności interfejsu
     MergeSort(0, LeftArray.size(), LeftArray);
     MergeSort(0, RightArray.size(), RightArray);
     Merge(LeftArray, RightArray, x, first);
@@ -40,7 +42,7 @@ void Merge(std::vector<T> &LeftArray, std::vector<T> &RightArray, std::vector<T>
     int r = 0;
 
     while (l < LeftSize && r < RightSize) {
-        if (LeftArray[l] < RightArray[r]) {
+        if (LeftArray[l] <= RightArray[r]) {  
             x[i] = LeftArray[l];
             i++;
             l++;
@@ -64,28 +66,47 @@ void Merge(std::vector<T> &LeftArray, std::vector<T> &RightArray, std::vector<T>
 }
 
 int main() {
-   
-
     // MergeSort test
     std::vector<int> x_merge = {5, -2, 64, 23, 87, 12, 45, 91, 3, 78, 56, 34, 29, 67, 41, 5, 88, 19, 72, 95, 11, 50};    
     std::vector<double> y_merge = {6.4, 2.3, 8.7, 1.2, 4.5, 9.1, 0.3, 7.8, 5.6, 3.4, 2.9, 6.7, 4.1, 0.5, 8.8, 1.9, 7.2, 9.5, 1.1, 5.0};  
 
-    std::vector<int> x_merge_test = x_merge;  
-    std::vector<double> y_merge_test = y_merge;  
+    // Test 1: Sortowanie całych tablic
+    std::vector<int> x_test1 = x_merge;  
+    std::vector<double> y_test1 = y_merge;  
 
-    MergeSort(3, x_merge_test.size(), x_merge_test);
-    MergeSort(0, y_merge_test.size(), y_merge_test);
+    MergeSort(0, x_test1.size(), x_test1);
+    MergeSort(0, y_test1.size(), y_test1);
 
-    std::cout << "MergeSort int (od indeksu 3) > ";
-    for (int i : x_merge_test) {
+    std::cout << "MergeSort int (cala tablica) > ";
+    for (int i : x_test1) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
-    std::cout << "MergeSort double > ";
-    for (double i : y_merge_test) {
+    
+    std::cout << "MergeSort double (cala tablica) > ";
+    for (double i : y_test1) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
+
+    // Test 2: Sortowanie częściowe
+    std::vector<int> x_test2 = x_merge;
+    MergeSort(3, 8, x_test2);  // Sortowanie fragmentu od indeksu 3 do 8
+
+    std::cout << "MergeSort int (indeksy 3-8) > ";
+    for (int i : x_test2) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    // Test uporządkowania
+    bool is_x_sorted = std::is_sorted(x_test1.begin(), x_test1.end());
+    bool is_y_sorted = std::is_sorted(y_test1.begin(), y_test1.end());
+    bool is_x_partial_sorted = std::is_sorted(x_test2.begin() + 3, x_test2.begin() + 8);
+
+    std::cout << "Vector x sorted: " << (is_x_sorted ? "YES" : "NO") << std::endl;
+    std::cout << "Vector y sorted: " << (is_y_sorted ? "YES" : "NO") << std::endl;
+    std::cout << "Vector x (partial 3-8) sorted: " << (is_x_partial_sorted ? "YES" : "NO") << std::endl;
 
     return 0;
 }
