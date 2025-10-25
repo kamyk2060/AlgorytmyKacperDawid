a3bbcc7cdb2f20e29e06472cdf2002070e43b34e
Kacper Dawid <thekamyk2060@gmail.com>
2025-10-25 15:47:55 +0200

    Poprawiono bubblesort

--- Zadanie2/BubbleSort/bubblesort.cpp
+++ Zadanie2/BubbleSort/bubblesort.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 template<typename T>
-void BubbleSort(int first, int last, std::vector<T> &x) {
-    T temp {};
-    for (int i = first; i < last; i++) {
-        for (int j = i + 1; j < last; j++) {
-            if (x[i] > x[j]) {
-                temp = x[j];
-                x[j] = x[i];
-                x[i] = temp;
+void BubbleSort(std::vector<T> &arr, int first, int last) {
+    bool swapped;
+    for (int i = first; i < last - 1; i++) {
+        swapped = false;
+        for (int j = first; j < last - (i - first) - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
+        }
+        // Jeśli w danej iteracji nie było żadnej zamiany, tablica jest już posortowana
+        if (!swapped) {
+            break;
         }
     }
 }
-
-
 
 int main() {
     std::vector<int> x {64, 23, 87, 12, 45, 91, 3, 78, 56, 34, 29, 67, 41, 5, 88, 19, 72, 95, 11, 50};    
@@ -26,18 +28,27 @@ int main() {
     std::vector<int> x_bubble = x;
     std::vector<double> y_bubble = y;
     
-    BubbleSort(0, x_bubble.size(), x_bubble);
-    BubbleSort(0, y_bubble.size(), y_bubble);
+    BubbleSort(x_bubble, 0, x_bubble.size());
+    BubbleSort(y_bubble, 0, y_bubble.size());
 
     std::cout << "BubbleSort int > ";
     for (int i : x_bubble) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
+    
     std::cout << "BubbleSort double > ";
     for (double i : y_bubble) {
         std::cout << i << " ";
     }
+    std::cout << std::endl;
+    
+    // Test uporządkowania
+    bool is_x_sorted = std::is_sorted(x_bubble.begin(), x_bubble.end());
+    bool is_y_sorted = std::is_sorted(y_bubble.begin(), y_bubble.end());
+    
+    std::cout << "Vector x is sorted: " << (is_x_sorted ? "YES" : "NO") << std::endl;
+    std::cout << "Vector y is sorted: " << (is_y_sorted ? "YES" : "NO") << std::endl;
     
     return 0;
 }
